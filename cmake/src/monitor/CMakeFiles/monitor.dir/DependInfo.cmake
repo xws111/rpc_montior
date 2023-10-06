e8402cedd561cb47700ea106b397b74dc743b80f
xws111 <18640173215@163.com>
2023-10-06 21:23:32 +0800

    ok

---
+++ cmake/src/monitor/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work/src/utils/read_file.cpp" "src/monitor/CMakeFiles/monitor.dir/__/utils/read_file.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/__/utils/read_file.cpp.o.d"
+  "/work/src/monitor/cpu_load_monitor.cpp" "src/monitor/CMakeFiles/monitor.dir/cpu_load_monitor.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/cpu_load_monitor.cpp.o.d"
+  "/work/src/monitor/cpu_softirq_monitor.cpp" "src/monitor/CMakeFiles/monitor.dir/cpu_softirq_monitor.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/cpu_softirq_monitor.cpp.o.d"
+  "/work/src/monitor/cpu_stat_monitor.cpp" "src/monitor/CMakeFiles/monitor.dir/cpu_stat_monitor.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/cpu_stat_monitor.cpp.o.d"
+  "/work/src/monitor/main.cpp" "src/monitor/CMakeFiles/monitor.dir/main.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/main.cpp.o.d"
+  "/work/src/monitor/mem_monitor.cpp" "src/monitor/CMakeFiles/monitor.dir/mem_monitor.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/mem_monitor.cpp.o.d"
+  "/work/src/monitor/net_monitor.cpp" "src/monitor/CMakeFiles/monitor.dir/net_monitor.cpp.o" "gcc" "src/monitor/CMakeFiles/monitor.dir/net_monitor.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/src/monitor/CMakeFiles/monitor.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monitor.dir/__/utils/read_file.cpp.o"
+  "CMakeFiles/monitor.dir/__/utils/read_file.cpp.o.d"
+  "CMakeFiles/monitor.dir/cpu_load_monitor.cpp.o"
+  "CMakeFiles/monitor.dir/cpu_load_monitor.cpp.o.d"
+  "CMakeFiles/monitor.dir/cpu_softirq_monitor.cpp.o"
+  "CMakeFiles/monitor.dir/cpu_softirq_monitor.cpp.o.d"
+  "CMakeFiles/monitor.dir/cpu_stat_monitor.cpp.o"
+  "CMakeFiles/monitor.dir/cpu_stat_monitor.cpp.o.d"
+  "CMakeFiles/monitor.dir/main.cpp.o"
+  "CMakeFiles/monitor.dir/main.cpp.o.d"
+  "CMakeFiles/monitor.dir/mem_monitor.cpp.o"
+  "CMakeFiles/monitor.dir/mem_monitor.cpp.o.d"
+  "CMakeFiles/monitor.dir/net_monitor.cpp.o"
+  "CMakeFiles/monitor.dir/net_monitor.cpp.o.d"
+  "monitor"
+  "monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
